61f948e0c007e32a3e833d7f8ec0996d1b45900e
Carlos Alberto Soto Pacheco <carlos.soto@epesa.local>
2021-12-03 14:56:28 -0600

    fb oath

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
     clientID: process.env.FB_ID,
     clientSecret: process.env.FB_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+    callbackURL: "https://secrets-ap.herokuapp.com/auth/facebook/secrets"
   },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ facebookId: profile.id }, function (err, user) {
